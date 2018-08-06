5a239c4a384cd0eedd931d451cc2088f2c2d33b0
rdgrv <doraagnesvarga@gmail.com>
2018-08-06 15:43:44 +0200

    Java tasks

---
+++ week-02/day-1/src/Matrix.java
@@ -0,0 +1,14 @@
+public class Matrix {
+}
+
+
+
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
