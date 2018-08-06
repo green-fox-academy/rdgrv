e0b2efec7e5d9a8da3001395b8824de2129de0d0
rdgrv <doraagnesvarga@gmail.com>
2018-08-06 17:00:46 +0200

    Java tasks

--- week-02/day-1/src/DoubleItems.java
+++ week-02/day-1/src/DoubleItems.java
@@ -9,7 +9,3 @@ public class DoubleItems {
     System.out.println(Arrays.toString(numList));
   }
 }
-
-// - Create an array variable named `numList`
-//   with the following content: `[3, 4, 5, 6, 7]`
-// - Double all the values in the array
