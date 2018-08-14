40bf050fdd443fada22fb20ff02f4ce14e98db85
rdgrv <doraagnesvarga@gmail.com>
2018-08-14 11:45:37 +0200

    create DivideByZero using void function

--- week-03/day-2/src/DivideByZero.java
+++ week-03/day-2/src/DivideByZero.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DivideByZero {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Give me a number: ");
+    int number = scanner.nextInt();
+    divide(number);
+  }
+
+  public static void divide(int number) {
+    int result = 0;
+    try {
+      result = 10 / number;
+      System.out.println(result);
+    } catch (ArithmeticException e) {
+      System.out.println("fail");
+    }
+  }
 }
