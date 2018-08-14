6a40d27e6a803bea1fa3b003798a264b66f992ea
rdgrv <doraagnesvarga@gmail.com>
2018-08-14 21:13:14 +0200

    created file and did main

--- week-03/day-2/src/TicTacToe.java
+++ week-03/day-2/src/TicTacToe.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TicTacToe {
+
+  public static void main(String[] args) {
+    String filename1 = "assets/win-o.txt";
+    Path path = Paths.get(filename1);
+    String filename2 = "assets/win-x.txt";
+    Path path = Paths.get(filename2);
+    String filename3 = "assets/draw.txt";
+    Path path = Paths.get(filename3);
+    System.out.println(ttt(filename1));
+    System.out.println(ttt(filename2));
+    System.out.println(ttt(filename3));
+  }
+
+  public static String ttt(String filename) {
+
+    // Write a function that takes a filename as string,
+    // open and read it. The file data represents a tic-tac-toe
+    // game result. Return 'X'/'O'/'draw' based on which player
+    // has winning situation.
+
+    System.out.println(ticTacResult("win-o.txt"))
+    // should print O
+
+    System.out.println(ticTacResult("win-x.txt"))
+    // should print X
+
+    System.out.println(ticTacResult("draw.txt"))
+    // should print draw
+  }
 }
