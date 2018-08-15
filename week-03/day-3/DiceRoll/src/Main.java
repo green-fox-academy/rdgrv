0b8b29c8e1f1916c57e99a618c617022f81b03fc
rdgrv <doraagnesvarga@gmail.com>
2018-08-15 19:04:21 +0200

    finished DiceSet

--- week-03/day-3/DiceRoll/src/Main.java
+++ week-03/day-3/DiceRoll/src/Main.java
@@ -11,10 +11,3 @@ public class Main {
     }
   }
 }
-
-
-//  You have a DiceSet class which has 6 dices
-//    You can roll all of them with roll()
-//    Check the current rolled numbers with getCurrent()
-//    You can reroll with reroll()
-//    Your task is to roll the dices until all of the dices are 6
