302ba694ec113f9bb98f58a68016e1d5b89ac620
rdgrv <doraagnesvarga@gmail.com>
2018-08-15 16:57:39 +0200

    create files

--- week-03/day-3/Dominoes/src/Main.java
+++ week-03/day-3/Dominoes/src/Main.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+  public static void main(String[] args) {
+    List<Domino> dominoes = initializeDominoes();
+
+    List<Domino> orderedDominoes = new ArrayList<>();
+
+    orderedDominoes.add(dominoes.remove(0));
+    for (int i = 0; i < orderedDominoes.size(); i++) {
+      for (int j = 0; j < dominoes.size(); j++) {
+        if (orderedDominoes.get(orderedDominoes.size() - 1).getValues()[1] == dominoes.get(j).getValues()[0]) {
+          orderedDominoes.add(dominoes.remove(j));
+        }
+      }
+    }
+
+    System.out.println(orderedDominoes);
+  }
+
+  static List<Domino> initializeDominoes() {
+    List<Domino> dominoes = new ArrayList<>();
+    dominoes.add(new Domino(5, 2));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(6, 7));
+    dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(7, 1));
+    return dominoes;
+  }
 }
