88a3274e0cb71c63d8bcbcc47467242db0168033
rdgrv <doraagnesvarga@gmail.com>
2018-08-15 16:56:37 +0200

    create files

---
+++ week-03/day-3/DiceRoll/src/Main.java
@@ -0,0 +1,8 @@
+public class DiceRoll {
+}
+
+//  You have a DiceSet class which has 6 dices
+//    You can roll all of them with roll()
+//    Check the current rolled numbers with getCurrent()
+//    You can reroll with reroll()
+//    Your task is to roll the dices until all of the dices are 6
