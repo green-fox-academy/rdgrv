df214336273a2e1a00cb402b7bcb4a1864466997
rdgrv <doraagnesvarga@gmail.com>
2018-08-15 11:54:28 +0200

    finished PostIt

--- week-03/day-3/PostIt/src/Main.java
+++ week-03/day-3/PostIt/src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Main {
+  public static void main(String[] args) {
+    PostIt postit1 = new PostIt();
+    postit1.backgroundColor = "orange";
+    postit1.textcolor = "blue";
+    postit1.text = "Idea 1";
+
+    PostIt postit2 = new PostIt();
+    postit2.backgroundColor = "pink";
+    postit2.textcolor = "black";
+    postit2.text = "Awesome";
+
+    PostIt postit3 = new PostIt();
+    postit3.backgroundColor = "yellow";
+    postit3.textcolor = "green";
+    postit3.text = "Superb";
+  }
 }
