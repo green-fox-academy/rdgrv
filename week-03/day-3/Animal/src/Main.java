f2e8a68d1486b978a7afccc894c534e367bbb7d6
rdgrv <doraagnesvarga@gmail.com>
2018-08-15 21:33:56 +0200

    finished Animal

---
+++ week-03/day-3/Animal/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    Animal duck = new Animal();
+    System.out.println(duck.hunger);
+    System.out.println(duck.thirst);
+    duck.eat();
+    duck.drink();
+    System.out.println(duck.hunger);
+    System.out.println(duck.thirst);
+    duck.play();
+    System.out.println(duck.hunger);
+    System.out.println(duck.thirst);
+  }
+}
