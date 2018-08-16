aeb410d376ff79a1e0489494e45cc396061f06b8
rdgrv <doraagnesvarga@gmail.com>
2018-08-16 11:57:41 +0200

    solved Bunnies2

---
+++ week-03/day-4/Bunnies2/src/Bunnies2.java
@@ -0,0 +1,15 @@
+public class Bunnies2 {
+  public static void main(String[] args) {
+    System.out.println(raisedFeet(5));
+  }
+
+  public static int raisedFeet (int n) {
+    if (n == 0) {
+      return 0;
+    } else if (n % 2 != 0) {
+      return 3 + raisedFeet(n - 1);
+    } else {
+      return 2 + raisedFeet(n - 1);
+    }
+  }
+}
