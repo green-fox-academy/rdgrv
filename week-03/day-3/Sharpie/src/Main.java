23cac967efb6e6a763e1e6ffc6563dd50345bdee
rdgrv <doraagnesvarga@gmail.com>
2018-08-20 20:25:22 +0200

    modified original Sharpie

--- week-03/day-3/Sharpie/src/Main.java
+++ week-03/day-3/Sharpie/src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    Sharpie sharpie = new Sharpie("green", 6.8);
-    sharpie.use();
-    System.out.println(sharpie.inkAmount);
+    Sharpie greenSharpie = new Sharpie("green", 6.8);
+    Sharpie yellowSharpie = new Sharpie("yellow", 7);
+
+    SharpieSet sharpieSet = new SharpieSet();
+    sharpieSet.add(greenSharpie);
+    sharpieSet.add(yellowSharpie);
+
+    for (int i = 0; i < 20; i++) {
+      greenSharpie.use();
+    }
+    System.out.println(greenSharpie.inkAmount);
+
+    System.out.println(sharpieSet.countUsable());
+    sharpieSet.removeTrash();
   }
 }
--- week-03/day-3/Sharpie/src/Sharpie.java
+++ week-03/day-3/Sharpie/src/Sharpie.java
@@ -4,10 +4,13 @@ public class Sharpie {
   double inkAmount;
 
   public Sharpie(String color, double width) {
-    this.inkAmount = 100;
+    inkAmount = 100;
   }
 
   public void use() {
     inkAmount -= 5.5;
+    if (inkAmount < 0) {
+      inkAmount = 0;
+    }
   }
 }
