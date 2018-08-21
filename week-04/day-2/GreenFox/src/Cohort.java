01da8e76ed4aa803bd9afc1f0b1ed4482a12570f
rdgrv <doraagnesvarga@gmail.com>
2018-08-21 13:46:21 +0200

    modified Cohort

--- week-04/day-2/GreenFox/src/Cohort.java
+++ week-04/day-2/GreenFox/src/Cohort.java
@@ -2,23 +2,26 @@ import java.util.ArrayList;
 
 public class Cohort {
   String name;
-  ArrayList<Student> students;
-  ArrayList<Mentor> mentors;
+  ArrayList<Student> students = new ArrayList<>();
+  ArrayList<Mentor> mentors = new ArrayList<>();
 
-  public void addStudent(Student student) {
-    students.add(student);
+  public ArrayList<Student> addStudent(Student student) {
+    this.students.add(student);
+    return students;
   }
 
-  public void addMentor(Mentor mentor) {
-    mentors.add(mentor);
+  public ArrayList<Mentor> addMentor(Mentor mentor) {
+    this.mentors.add(mentor);
+    return mentors;
   }
 
   public void info() {
-    System.out.println("The " + name + "cohort has " + students.size() + " students and " + mentors.size() + " mentors.");
+    System.out.println("The " + name + " cohort has " + students.size() + " students and " + mentors.size() + " mentors.");
   }
 
   public Cohort(String name){
-    ArrayList<Student> students;
-    ArrayList<Mentor> mentors;
+    this.name = name;
+    this.students = students;
+    this.mentors = mentors;
   }
 }
