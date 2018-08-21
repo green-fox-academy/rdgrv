b5bf2d0c5094143ca0e2aa70740c063f6675b2d1
rdgrv <doraagnesvarga@gmail.com>
2018-08-21 13:46:36 +0200

    modified Mentor

--- week-04/day-2/GreenFox/src/Mentor.java
+++ week-04/day-2/GreenFox/src/Mentor.java
@@ -11,12 +11,12 @@ public class Mentor extends Person{
   }
 
   public Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
   }
 
   public Mentor() {
-    name = "Jane Doe";
-    age = 30;
-    gender = "female";
+    super("Jane Doe", 30, "female");
     level = "intermediate";
   }
 }
