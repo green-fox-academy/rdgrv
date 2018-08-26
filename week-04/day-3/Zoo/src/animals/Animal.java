9bf2cd386112e781760a63b863167c35f0c2fc2b
rdgrv <doraagnesvarga@gmail.com>
2018-08-26 19:10:13 +0200

    created Animal

---
+++ week-04/day-3/Zoo/src/animals/Animal.java
@@ -0,0 +1,33 @@
+package animals;
+
+public abstract class Animal {
+  String name;
+  int age;
+  String gender;
+  boolean cuteness;
+
+  public Animal(String name, int age, String gender, boolean cuteness) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.cuteness = cuteness;
+  }
+
+  abstract String breed();
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public boolean isItCute() {
+    return cuteness;
+  }
+}
