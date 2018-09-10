6c102cba44d775bc7d13cebeab10c7a781789649
rdgrv <doraagnesvarga@gmail.com>
2018-09-10 13:57:05 +0200

    added exercise 8

---
+++ week-06/day-1/src/Ex8.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Ex8 {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+    System.out.println(numbers.stream()
+        .collect(Collectors.groupingBy(c -> c, Collectors.counting())));
+
+  }
+}
