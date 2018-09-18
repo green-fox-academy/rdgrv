3bb3d559cef22125c92f99912f3aa8d399f1385b
rdgrv <doraagnesvarga@gmail.com>
2018-09-18 15:03:12 +0200

    did exercise 'HTMLception'

--- week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
+++ week-07/day-2/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
@@ -9,10 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BankAccountController {
   BankAccount bankAccount = new BankAccount("Simba", new Balance(2000, "zebra"), "lion");
+  String htmlception = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
+
 
   @RequestMapping("/show")
-  public String greeting(Model model) {
+  public String bankaccount(Model model) {
     model.addAttribute("bankaccount", bankAccount);
     return "show";
+  }
+
+  @RequestMapping("/htmlception")
+  public String htmlception(Model model) {
+    model.addAttribute("htmlception", htmlception);
+    return "htmlception";
   }
 }
