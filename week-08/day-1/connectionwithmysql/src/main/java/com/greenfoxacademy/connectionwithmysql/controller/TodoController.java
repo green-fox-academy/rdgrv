e68883225760715f525e9040ca7e065646296815
rdgrv <doraagnesvarga@gmail.com>
2018-09-25 16:21:19 +0200

    added deleting feature

--- week-08/day-1/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodoController.java
+++ week-08/day-1/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,6 +40,12 @@ public class TodoController {
   @PostMapping("/add")
   public String postAdd(@RequestParam(value = "title") String title) {
     todoRepository.save(new Todo(title));
+    return "redirect:/list";
+  }
+
+  @GetMapping("/{id}/delete")
+  public String delete(@PathVariable(value = "id") long id) {
+    todoRepository.deleteById(id);
     return "redirect:/list";
   }
 }
