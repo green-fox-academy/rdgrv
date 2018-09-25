893a00946ee4239fe96e99967cd55d707fae661b
rdgrv <doraagnesvarga@gmail.com>
2018-09-25 14:22:36 +0200

    added mySQL

---
+++ week-08/day-1/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.connectionwithmysql.repository;
+
+import com.greenfoxacademy.connectionwithmysql.model.Todo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+}
