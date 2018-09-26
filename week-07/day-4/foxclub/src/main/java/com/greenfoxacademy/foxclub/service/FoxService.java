623a229eebde4f3ea8b5672df7008bb52e0d3060
rdgrv <doraagnesvarga@gmail.com>
2018-09-26 09:32:02 +0200

    modified foxclub

--- week-07/day-4/foxclub/src/main/java/com/greenfoxacademy/foxclub/service/FoxService.java
+++ week-07/day-4/foxclub/src/main/java/com/greenfoxacademy/foxclub/service/FoxService.java
@@ -4,6 +4,7 @@ import com.greenfoxacademy.foxclub.model.Fox;
 import com.greenfoxacademy.foxclub.repository.FoxRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
